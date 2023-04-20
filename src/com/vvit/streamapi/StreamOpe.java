2aa4c90e5ef166da6fec11206a47de5c18580bc8
Pavankumar Hegde <puhegde01@gmail.com>
2023-04-20 16:54:32 +0530

    added stream api

--- src/com/vvit/streamapi/StreamOpe.java
+++ src/com/vvit/streamapi/StreamOpe.java
@@ -1,13 +1,27 @@
 package com.vvit.streamapi;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class StreamOpe {
 	public static void main(String[] args) {
-		List<String> countries = Arrays.asList("INDIA","USA","UK");
-		System.out.println(countries);
+	LinkedList<Integer> lang = new LinkedList<Integer>();
+	lang.add(8);
+	lang.add(2);
+	lang.add(1);
+	lang.add(6);
+	
+	
+	Iterator it = lang.iterator();
+	Collections.reverse(lang);
+	Collections.sort(lang);
+	while(it.hasNext());
+	System.out.println(it.next() + " ");
+	
 		
 	}
 
